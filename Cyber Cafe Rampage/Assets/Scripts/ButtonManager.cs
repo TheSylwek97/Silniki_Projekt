cb07c2b38c97b49a6d78b406e6f1b19c3ff6675d
TheSylwek97 <38591912+TheSylwek97@users.noreply.github.com>
2018-12-14 20:50:05 +0100

    Można z menu włączać 1. scenę.

---
+++ Cyber Cafe Rampage/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonManager : MonoBehaviour {
+
+	public void NewGameBtn(string NewGameLevel1)
+    {
+        SceneManager.LoadScene(NewGameLevel1);
+    }
+
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+}
