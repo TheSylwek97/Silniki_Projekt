ecbbed80c0678910c53691da8b00daf590651926
KlaudikX <45332552+KlaudikX@users.noreply.github.com>
2018-12-14 20:58:34 +0100

    Gdy klikasz na wybrany produkt podaza on za myszka i znika po najechaniu nim na ekspres. Dodalam tez brakujace prefaby :D

---
+++ Cyber Cafe Rampage/Assets/Scrypty/MoveScrypt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScrypt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y); //pobieramy sobie pozycję myszki
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); //musi to być, żeby działało, bo Unity. Przynajmniej tak zrozumiałam
+            transform.position = objPosition; //rusza się
+                                              // GetComponent<Rigidbody2D>().simulated = true; //dajemy simulated na true, żeby znikał nam obiekt, jeżeli cały czas bedzie true to spadnie ze sceny
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "CoffeeMaker") //A tutaj gdy najezdrzamy na obiekt "ekspress" to obiekt znika ze sceny
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
