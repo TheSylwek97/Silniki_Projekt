9857338394506e9bec806997b9b3628cc3ec54ee
KlaudikX <45332552+KlaudikX@users.noreply.github.com>
2018-12-15 21:46:41 +0100

    Dodane do ekspressu składniki są dodawane do listy :P

---
+++ Cyber Cafe Rampage/Assets/Scrypty/ListOfIngredients.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ListOfIngredients : MonoBehaviour {
+
+    //Recipe01 Recipe;
+
+    public List<string> ListOfItem = new List<string>();
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        string name = collider.gameObject.name;
+        ListOfItem.Add(name);
+       
+    }
+
+    public class IsCorrest
+    {
+      
+    }
+}
---
+++ Cyber Cafe Rampage/Assets/Scrypty/Recipe01.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipe01 : MonoBehaviour {
+
+    
+    public void Recipe ()
+    {
+        List<string> Recipe01;
+        Recipe01.Add("Milk");
+        Recipe01.Add("Sugar");
+    } 
+}
