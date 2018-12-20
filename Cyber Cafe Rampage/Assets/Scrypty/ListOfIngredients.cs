447dbd26af3fe14f2ba35eefecdd9a09a25fd489
KlaudikX <45332552+KlaudikX@users.noreply.github.com>
2018-12-20 21:40:09 +0100

    Zrobiłam porządek w Prefabach i dodałam kolejność dodawania składników

--- Cyber Cafe Rampage/Assets/Scrypty/ListOfIngredients.cs
+++ Cyber Cafe Rampage/Assets/Scrypty/ListOfIngredients.cs
@@ -5,33 +5,75 @@ using System.Linq;
 
 public class ListOfIngredients : MonoBehaviour {
 
-
+    public int number = 0;
+    public int _number = 0;
     public List<string> RecipeList = new List<string>();
     public List<string> ListOfItem = new List<string>();
     [SerializeField]
     public GameObject level;
-    public int number = 0;
-    public int _number = 0;
-    // Use this for initialization
+    
+    
     void Start () {
         string _tag = level.tag;
 
         if (_tag == "Level01")
         {
+            RecipeList.Add("Honey");
+            RecipeList.Add("Cinnamon");
             RecipeList.Add("Milk");
-            RecipeList.Add("Sugar");
-            _number = 2;
+            RecipeList.Add("Espresso");
+            _number = 4;
             
         }
         if (_tag == "Level02")
         {
+            RecipeList.Add("Kakao");
             RecipeList.Add("Sugar");
+            RecipeList.Add("Chili");
+            RecipeList.Add("Cinnamon");
+            _number = 4;
+
         }
+        if (_tag == "Level03")
+        {
+            RecipeList.Add("Milk");
+            RecipeList.Add("White Chocolate");
+            RecipeList.Add("Amaretto");
+            RecipeList.Add("Vanilla");
+            RecipeList.Add("Espresso");
+            _number = 5;
+        }
+        if (_tag == "Level04")
+        {
+            RecipeList.Add("Milk");
+            RecipeList.Add("Espresso");
+            RecipeList.Add("Maple syrup");
+            RecipeList.Add("Gingerbread spice");
+            RecipeList.Add("Whipped cream");
+            RecipeList.Add("Gingerbread");
+            _number = 6;
+
+        }
+        if (_tag == "Level05")
+        {
+            RecipeList.Add("Milk");
+            RecipeList.Add("Pumpkin puree");
+            RecipeList.Add("Sugar");
+            RecipeList.Add("Cinnamon");
+            RecipeList.Add("Nutmeg");
+            RecipeList.Add("Ginger");
+            RecipeList.Add("Carnation");
+            RecipeList.Add("Espresso");
+            RecipeList.Add("Vanilla");
+            RecipeList.Add("Whipped cream");
+            _number = 10;
+
+        }
+       
     }
-	
-	// Update is called once per frame
+
 	void Update () {
-        IsCorrest();
+        IsCorrect();
     }
 
     public void OnTriggerEnter2D(Collider2D collider)
@@ -42,7 +84,7 @@ public class ListOfIngredients : MonoBehaviour {
 
     }
 
-    public void IsCorrest()
+    public void IsCorrect()
     {
         if (number == _number)
         {
--- Cyber Cafe Rampage/Assets/Scrypty/MoveScrypt.cs
+++ Cyber Cafe Rampage/Assets/Scrypty/MoveScrypt.cs
@@ -5,12 +5,7 @@ using UnityEngine;
 public class MoveScrypt : MonoBehaviour {
 
     bool status;
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
+    
 	void Update () {
 
         if(status == true)
