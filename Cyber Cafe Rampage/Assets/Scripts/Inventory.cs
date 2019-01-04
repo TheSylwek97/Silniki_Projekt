6ee4dbc6d47596ce745ef75e9d96812f099ccedc
TheSylwek97 <38591912+TheSylwek97@users.noreply.github.com>
2019-01-04 22:07:12 +0100

    Zapisuje liste rzeczy do inventory po kliknięciu

--- Cyber Cafe Rampage/Assets/Scripts/Inventory.cs
+++ Cyber Cafe Rampage/Assets/Scripts/Inventory.cs
@@ -5,77 +5,8 @@ using System.IO;
 
 public class Inventory : MonoBehaviour
 {
-    public List<string> ListaItemow = new List<string>();
-    //public int _click = 1;
-    //public GameObject item;
-    //private GameController gc;
-    public bool isClick;
-
-    public void Start()
-    {
-        //gc = GameObject.Find("GameController").GetComponent<GameController>();
-    }
-
-    public void Update()
-    {
-
-        AddItem();
-    }
-
-    public void AddItem()
-    {
-        MoveScrypt cs = gameObject.GetComponentInChildren<MoveScrypt>();
-        if (cs.tag == "Item")
-        {
-            ListaItemow.Add(cs.name);
-            Debug.Log("zostalo dodane");
-            Destroy(cs.gameObject);
-        }
-    }
-
-    /*public void Tom(bool click)
-    {
-        if (click = true)
-        {
-            isClick = true;
-        }
-    
-    }*/
-
-    public void SetParent()
-    {
-        //item.transform.SetParent(newParent);
-        /*if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = gameObject.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.transform.gameObject.tag == "Player")
-                {
-                    Transform selected = hit.transform;
-                    gc.unselectAll();
-                    selected.SendMessage("selectPlayer");
-                }
-            }
-        }*/
-    }
-
-    /*private void OnEnable()
-    {
-        var isom = File.ReadAllText(Application.persistentDataPath + '/' + "save.dat");
-        Data = JsonUtility.FromJson<InventoryData>(isom);
+    public List<GameObject> ListaItemow = new List<GameObject>();
 
 
-    }
-    private void OnDisable()
-    {
-
-        var value = JsonUtility.ToJson(Data);
-        Debug.Log(value);
-        File.WriteAllText(Application.persistentDataPath + '/' + "save.dat", value);
-    }*/
-
-    
 
 }
--- Cyber Cafe Rampage/Assets/Scripts/MoveScrypt.cs
+++ Cyber Cafe Rampage/Assets/Scripts/MoveScrypt.cs
@@ -2,38 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveScrypt : MonoBehaviour {
+public class MoveScrypt : MonoBehaviour
+{
 
     bool status;
-    public bool click;
-    public bool mom;
-    public bool hover = false;
-    public GameObject level;
-   // public GameObject item;
-    //private Transform playerSelection;
-   // private bool isSelected;
+    public Inventory level;
 
     public void Start()
     {
+        level = GameObject.Find("GameMaster").GetComponent<Inventory>();
+
         if (level.tag == "Messroom")
         {
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
             rb.constraints = RigidbodyConstraints2D.FreezePosition;
 
-            
         }
     }
-    public void Update ()
+    public void Update()
     {
-         if (status == true)
-         {
-                Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-                Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-                transform.position = objPosition;
-         }
-        if ((hover == true) && (Input.GetMouseButtonDown(0)))
+        if (status == true)
         {
-           // click = true;
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objPosition;
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
@@ -46,88 +38,21 @@ public class MoveScrypt : MonoBehaviour {
 
     public void OnMouseDown()
     {
-       
         if (level.tag != "Messroom")
-        {
             status = true;
-        }
+
         if ((level.tag == "Messroom"))
-        {
-           
-            
-            
-        }
-        else
-        {
-            
-        }
+            level.ListaItemow.Add(this.gameObject);
     }
 
-    public void OnMouseDrag()
-    {
-        
-    }
-
-    
-    
     public void OnMouseUp()
     {
         if (level.tag != "Messroom")
-        {
             status = false;
-        }
+
         if (level.tag == "Messroom")
-        {
             status = false;
-        }
-        if(tag == "Item")
-        {
-            this.gameObject.SetActive(true);
-        }
-        if(tag != "Item")
-        {
-            //this.gameObject.
-        }
     }
 
-    public void OnMouseOver()
-    {
-        tag = "Item";
-    }
-
-    public void OnMouseExit()
-    {
-        tag = "Untagged";
-    }
-
-    /* public void IsClick(bool mom)
-     {
-         if(mom == true)
-         {
-             click = true;
-             Debug.Log("Zorro");
-         }
-     }*/
-
-    /*public void OnMouseOver(GameObject newParent)
-    {
-        hover = true;
-        //newParent = gameObject.transform;
-        // level.transform.SetChot(newParent.transform);
-        item.transform.parent = newParent.transform;
-        
-    }*/
-
-    /* public void selectPlayer()
-     {
-         isSelected = true;
-         playerSelection.gameObject.SetActive(true);
-     }
-
-     public void unselectPlayer()
-     {
-         isSelected = false;
-         playerSelection.gameObject.SetActive(false);
-     }*/
 
 }
