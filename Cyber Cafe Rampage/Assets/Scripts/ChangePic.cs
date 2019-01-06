062a7fb4c55e84e4c9935ebb3d1c2ccd6938d861
TheSylwek97 <38591912+TheSylwek97@users.noreply.github.com>
2019-01-06 20:57:14 +0100

    Skrypt zmieniający prefaby na inne -> wyłącza 1. włącza 2.

---
+++ Cyber Cafe Rampage/Assets/Scripts/ChangePic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePic : MonoBehaviour {
+
+
+    [SerializeField]
+    public GameObject oldPic, newPic;
+
+    void Start()
+    {
+        oldPic.GetComponent<GameObject>();
+        newPic.GetComponent<GameObject>();
+        newPic.gameObject.active = false;
+    }
+    public void OnMouseDown()
+    {
+        oldPic.gameObject.active = false;
+        newPic.gameObject.active = true;
+
+    }
+
+}
