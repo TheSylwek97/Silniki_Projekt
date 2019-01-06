9ebe15856f6e8eadab95802fd2de7c3fd776e23e
TheSylwek97 <38591912+TheSylwek97@users.noreply.github.com>
2019-01-06 21:11:16 +0100

    Animacje też sie uruchamiają razem z zaminą obrazka. Na razie OnMouseDown

--- Cyber Cafe Rampage/Assets/Scripts/ChangePic.cs
+++ Cyber Cafe Rampage/Assets/Scripts/ChangePic.cs
@@ -7,17 +7,25 @@ public class ChangePic : MonoBehaviour {
 
     [SerializeField]
     public GameObject oldPic, newPic;
+    public GameObject fire, steam;
 
     void Start()
     {
         oldPic.GetComponent<GameObject>();
         newPic.GetComponent<GameObject>();
+        fire.GetComponent<GameObject>();
+        steam.GetComponent<GameObject>();
         newPic.gameObject.active = false;
+        fire.gameObject.active = false;
+        steam.gameObject.active = false;
+
     }
     public void OnMouseDown()
     {
         oldPic.gameObject.active = false;
         newPic.gameObject.active = true;
+        fire.gameObject.active = true;
+        steam.gameObject.active = true;
 
     }
 
