996732ff1acf3ad7b09449dcbf1bc08c4e70d35d
TheSylwek97 <38591912+TheSylwek97@users.noreply.github.com>
2019-01-06 16:32:29 +0100

    Dodany licznik do pokazania przepisów

---
+++ Cyber Cafe Rampage/Assets/Scripts/IngredientsTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class IngredientsTimer : MonoBehaviour {
+
+
+
+    [SerializeField]
+    public string levelName;
+    public float setTime;
+    public float runTime = 0f;
+
+    public void Update()
+    {
+        runTime += Time.deltaTime;
+        if (runTime>= setTime)
+        {
+             SceneManager.LoadScene(levelName);
+        }
+
+    }
+}
