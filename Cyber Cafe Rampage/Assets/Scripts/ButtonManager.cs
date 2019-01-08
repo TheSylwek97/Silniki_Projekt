296b3cd5a5c0b47679b690de87849e0a6f36ead8
TheSylwek97 <38591912+TheSylwek97@users.noreply.github.com>
2019-01-08 18:14:38 +0100

    DZIAŁA! Jest zapis gry i pamięta go po po wyłączeniu playmode. PlayerPref

--- Cyber Cafe Rampage/Assets/Scripts/ButtonManager.cs
+++ Cyber Cafe Rampage/Assets/Scripts/ButtonManager.cs
@@ -11,11 +11,35 @@ public class ButtonManager : MonoBehaviour {
     }
     public void MenuBtn()
     {
+       /* if(PlayerData.whichLvl > 0)
+        {
+            PlayerData.whichLvl = 0;
+        }*/
         SceneManager.LoadScene("MenuStart");
     }
     public void LoadGameBtn()
     {
-        
+        if(PlayerData.whichLvl == 2)
+        {
+            SceneManager.LoadScene("Ingredients_Level02");
+        }
+        else if (PlayerData.whichLvl == 3)
+        {
+            SceneManager.LoadScene("Ingredients_Level03");
+        }
+        else if (PlayerData.whichLvl == 4)
+        {
+            SceneManager.LoadScene("Ingredients_Level04");
+        }
+        else if (PlayerData.whichLvl == 5)
+        {
+            SceneManager.LoadScene("Ingredients_Level05");
+        }
+        else
+        {
+            SceneManager.LoadScene("Ingredients_Level01");
+        }
+
     }
     public void ExitGameBtn()
     {
