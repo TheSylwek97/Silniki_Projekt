23a241509442ca07366d69575602bc78e235646d
TheSylwek97 <38591912+TheSylwek97@users.noreply.github.com>
2019-01-08 14:39:13 +0100

    Jeszcze raz

--- Cyber Cafe Rampage/Assets/Scripts/ButtonManager.cs
+++ Cyber Cafe Rampage/Assets/Scripts/ButtonManager.cs
@@ -9,10 +9,22 @@ public class ButtonManager : MonoBehaviour {
         SceneManager.LoadScene(nextSecne);
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    public void MenuBtn()
+    {
+        SceneManager.LoadScene("MenuStart");
+    }
+    public void LoadGameBtn()
+    {
+        
+    }
     public void ExitGameBtn()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void SaveGameBtn(int levelCommp)
+    {
+        Save.level = levelCommp;
     }
 }
