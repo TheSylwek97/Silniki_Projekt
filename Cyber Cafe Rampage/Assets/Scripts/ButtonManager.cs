de77213291bde8d265e3db60a62766af82a2c7f1
TheSylwek97 <38591912+TheSylwek97@users.noreply.github.com>
2019-01-08 01:37:12 +0100

    Zapis gry próbny

--- Cyber Cafe Rampage/Assets/Scripts/ButtonManager.cs
+++ Cyber Cafe Rampage/Assets/Scripts/ButtonManager.cs
@@ -10,9 +10,53 @@ public class ButtonManager : MonoBehaviour {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void NewSecneBtn()
+    {
+        SaveGame.levelCompleted = 0;
+        SceneManager.LoadScene("Ingredients_Level01");
+    }
+
+    public void LoadSecneBtn()
+    {
+        if (SaveGame.levelCompleted == 1)
+        {
+            SceneManager.LoadScene("Ingredients_Level02");
+        }
+        else if (SaveGame.levelCompleted == 2)
+        {
+            SceneManager.LoadScene("Ingredients_Level03");
+
+        }
+        else if (SaveGame.levelCompleted == 3)
+        {
+            SceneManager.LoadScene("Ingredients_Level04");
+
+        }
+        else if (SaveGame.levelCompleted == 4)
+        {
+            SceneManager.LoadScene("Ingredients_Level05");
+        }
+        else
+        {
+            SceneManager.LoadScene("Ingredients_Level01");
+        }
+    }
+    public void MenuBtn()
+    {
+        SceneManager.LoadScene("MenuStart");
+    }
+
+
     public void ExitGameBtn()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    private void Update()
+    {
+        //SaveGame.ReadLevel();
+
+        //Debug.Log(SaveGame.levelCompleted);
     }
 }
--- Cyber Cafe Rampage/Assets/Scripts/SaveGame.cs
+++ Cyber Cafe Rampage/Assets/Scripts/SaveGame.cs
@@ -5,4 +5,37 @@ using UnityEngine;
 public class SaveGame : MonoBehaviour {
 
     //PlayerPrefs.SetInt("Player Score", 10);
+    //[SerializeField]
+    public static int levelCompleted=0;
+
+    public void Start()
+    {
+        ReadLevel();
+        Debug.Log(levelCompleted);
+        levelCompleted++;
+        ReadLevel();
+        Debug.Log(levelCompleted);
+    }
+
+    public static void SetLevel()
+    {
+        PlayerPrefs.SetInt("WhichLevel", levelCompleted);
+        PlayerPrefs.Save();
+    }
+
+    public static void ReadLevel()
+    {
+        levelCompleted = PlayerPrefs.GetInt("levelCompleted");
+    }
+
+    public static void CheckKey()
+    {
+        PlayerPrefs.HasKey("levelCompleted");
+    }
+
+    public static void ResetLevel()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+        
 }
